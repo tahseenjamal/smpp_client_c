9fad06de4404eac097424709d5f57564441a98fd
Tahseen Jamal <tahseen.jamal@gmail.com>
2026-03-13 15:14:56 +0530

    Added conf file along with *.c and *.h to generate report

--- code.bash
+++ code.bash
@@ -1,6 +1,6 @@
 find . \
   -type d \( -name target -o -name .git \) -prune -o \
-  -type f \( -name "*.c" -o -name "*.h" \) -print | while read -r f; do
+  -type f \( -name "*.c" -o -name "*.h" -o -name "*.conf" \) -print | while read -r f; do
     echo "===== $f ====="
     cat "$f"
     echo
